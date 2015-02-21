db75219f324ad72266312d1c031feda768d984a4
Bill Salefski <Bill@Bill-Salefskis-iMac-2.local>
2015-02-21 17:52:46 +0200

    Initial Commit

---
+++ SortingTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SortingTest
+//
+//  Created by Bill Salefski on 21-Feb-15.
+//  Copyright (c) 2015 Bill Salefski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
